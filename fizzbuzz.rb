4d8087bd4d1a901e6f7e7c71b66eda937d02612d
neah101 <neah.shaw2005@gmail.com>
2018-06-19 20:06:05 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,22 @@
+def fizz_buzz(int)
+  if int%15==0
+    puts "fizz_buzz"
+  
+  elsif int%5==0
+    puts "buzz"
+  
+  elsif int%3==0 
+    puts "fizz"
+ 
+ 
+  else 
+    puts " not right sorry try again with another number"
+  end
+end
+  
+  fizz_buzz(14)
+  fizz_buzz(15)
+  fizz_buzz(20)
+  fizz_buzz(12) 
+  
+  
